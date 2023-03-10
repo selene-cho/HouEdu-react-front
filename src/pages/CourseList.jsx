af0b7cb25defd0fd5b7d81b7593dff262ad0bb15
selene-cho <selene.cho29@gmail.com>
2023-03-10 13:50:32 +0900

    delete: CourseList 내용 삭제

--- src/pages/CourseList.jsx
+++ src/pages/CourseList.jsx
@@ -1,30 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export default function CourseList() {
-  const [text, setText] = useState('');
-  const navigate = useNavigate(); // 코드상에서 동적으로 이동
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setText('');
-    navigate(`/course/${text}`);
-  };
-
-  return (
-    <div>
-      Courses{' '}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="course id: "
-          value={text}
-          onChange={handleChange}
-        />
-      </form>
-    </div>
-  );
+  return <div>Courses</div>;
 }
