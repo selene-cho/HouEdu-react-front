1e3bf2c4d67e9ed63f535019d3b9e5d5c057809b
im ChangSeÖp <ggggjkas@gmail.com>
2023-03-10 16:40:54 +0900

    Course 폴더 안 1. Portfolio 컴포넌트 및 scss 스타일 수정 완료 추후 기능추가 예정만 있습니다. 2. Search 컴포넌트, scss 생성 금일 검색 기능및 스타일 업데이트 했습니다.

--- src/components/course/Portfolios.jsx
+++ src/components/course/Portfolios.jsx
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './Portfolio.module.scss';
 import dummy from '../../dummy/portfolio.json';
 
 export default function Portfolios() {
    console.log(dummy);
-   const [pofol, setPofol] = useState([]);
 
    return (
-      <div className={styles.container}>
-         <div className={styles.main}>
-            <h1 className={styles.title}>수강생의 작품</h1>
-            <div className={styles.items}>
-               <div className={styles.item}>
-                  <img />
-                  <p>title</p>
-                  <p>info</p>
+      <section className={styles.container}>
+         {dummy.items.map(item => (
+            <div key={item.id} className={styles.main}>
+               <img className={styles.item} src={item.url} alt={item.name} />
+               <div className={styles.items}>
+                  <h3 className={styles.title}>{item.name}</h3>
+                  <p className={styles.date}>{item.date}</p>
                </div>
             </div>
-         </div>
-      </div>
+         ))}
+      </section>
    );
 }
--- src/pages/CourseList.jsx
+++ src/pages/CourseList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Portfolios from '../components/course/Portfolios';
+import SearchCourse from '../components/course/SearchCourse';
+import styles from './CourseList.module.scss';
 
 export default function CourseList() {
    return (
-      <div>
+      <div className={styles.container}>
          <Portfolios />
+         <SearchCourse />
       </div>
    );
 }
