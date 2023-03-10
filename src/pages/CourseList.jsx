6d67a8af8aa03b479569de474234ccde0371b203
selene-cho <selene.cho29@gmail.com>
2023-03-10 17:42:16 +0900

    fix: courselist 초기화

--- src/pages/CourseList.jsx
+++ src/pages/CourseList.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import Portfolios from '../components/course/Portfolios';
-import SearchCourse from '../components/course/SearchCourse';
-import styles from './CourseList.module.scss';
 
 export default function CourseList() {
-   return (
-      <div className={styles.container}>
-         <Portfolios />
-         <SearchCourse />
-      </div>
-   );
+  return <div>CourseList</div>;
 }
