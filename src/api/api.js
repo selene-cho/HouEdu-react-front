51bb53e5a77a9c60a6aa35b5a718f795320c6c01
selene-cho <selene.cho29@gmail.com>
2023-03-10 15:21:54 +0900

    fix: api base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,15 @@
 // import axios from 'axios';
 
 // const instance = axios.create({
-//   baseURL: 'http://127.0.0.1:8000/api/v1/',
-//   headers: {
-//     'X-CSRFToken': Cookies.get('csrftoken'),
-//   },
+//   baseURL: 'http://3.35.67.72:8000/api/v1/',
+//   // headers: {
+//   //   'X-CSRFToken': Cookies.get('csrftoken'),
+//   // },
 //   withCredentials: true,
 // });
+
 // export const baseUrl = 'http://3.35.67.72:8000/api/v1/courses/'
+
+// export const CourseList = () => {
+//   return instance.get('courses/').then((res) => res.data);
+// };
