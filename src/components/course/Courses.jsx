7163e42a01252c89e363558a319c10b9d423b204
im ChangSeÖp <ggggjkas@gmail.com>
2023-03-13 15:41:06 +0900

    Revert "Revert "Revert "전체리스트 업데이트"""
    This reverts commit d6301fd905bea9097b6fe886fd580ce817f91f90.

--- src/components/course/Courses.jsx
+++ src/components/course/Courses.jsx
@@ -8,6 +8,11 @@ const lectures = data.lecture;
 export default function Courses() {
    const [showCount, setShowCount] = useState(4);
    const { courseId } = useParams();
+   const navigator = useNavigate();
+
+   const handleNav = () => {
+      navigator(`/courses/${courseId}`);
+   };
 
    const handleShowMore = () => {
       setShowCount(showCount + 4);
@@ -19,9 +24,9 @@ export default function Courses() {
             .filter(courses => !courseId || courses.id === courseId) // 필터링 조건 수정
             .slice(0, showCount)
             .map(courses => (
-               <Link to={`/courses/${courses.id}`}>
+               <Link to={`/courses/${courses.id}`} key={courses.id}>
                   {/* Link에 key 추가 */}
-                  <div className={styles.courses} key={courses.id}>
+                  <div className={styles.courses} onClick={handleNav}>
                      <img className={styles.crs__image} src={courses.url} alt={`courses ${courses.id}`} />
                      <p>{courses.crs_name}</p>
                      <p>{courses.crs_info}</p>
