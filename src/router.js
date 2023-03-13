75f23586f8aa7a708bbe80a2a3068068597a20fa
im ChangSeÖp <ggggjkas@gmail.com>
2023-03-13 15:03:36 +0900

    Revert "Revert "전체 리스트""
    This reverts commit db8dcce4f0a060798c52b220852deb879ca3baca.

--- src/router.js
+++ src/router.js
@@ -5,8 +5,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import CourseList from './pages/CourseList';
-import Lectures from './pages/Lectures';
-import NewLecture from './pages/NewLecture';
 import Reviews from './pages/Reviews';
 import Portfolio from './pages/Portfolio';
 import MyCart from './pages/MyCart';
@@ -14,36 +12,36 @@ import NotFound from './pages/NotFound';
 import Login from './components/auth/Login';
 import KakaoLogin from './components/auth/KakaoLogin';
 import SignUp from './components/auth/SignUp';
+import CourseDetail from './pages/CourseDetail';
 
 // const isLoggedIn = useReactiveVar(isLoggedInVar);
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />, // 사용자 지정 Error Page
-    children: [
-      { index: true, path: '/', element: <Home /> },
-      { path: '/courses', element: <CourseList /> },
-      { path: '/courses/new', element: <NewLecture /> },
-      { path: '/course/:courseId', element: <Lectures /> },
-      { path: '/reviews', element: <Reviews /> },
-      { path: '/portfolio', element: <Portfolio /> },
-      { path: '/carts', element: <MyCart /> },
-    ],
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/kakaologin',
-    element: <KakaoLogin />,
-  },
+   {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFound />, // 사용자 지정 Error Page
+      children: [
+         { index: true, path: '/', element: <Home /> },
+         { path: '/course', element: <CourseList /> },
+         { path: '/courses/:coursesId', element: <CourseDetail /> },
+         { path: '/reviews', element: <Reviews /> },
+         { path: '/portfolio', element: <Portfolio /> },
+         { path: '/carts', element: <MyCart /> },
+      ],
+   },
+   {
+      path: '/signup',
+      element: <SignUp />,
+   },
+   {
+      path: '/login',
+      element: <Login />,
+   },
+   {
+      path: '/kakaologin',
+      element: <KakaoLogin />,
+   },
 ]);
 
 export default router;
