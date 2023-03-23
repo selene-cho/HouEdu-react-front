8e835124e73444a43c869543b5f4a59788c1f145
im ChangSeÖp <ggggjkas@gmail.com>
2023-03-23 18:15:45 +0900

    api사용전 전체리스트 ui변경 상세페이지 스타일  변경

--- src/components/course/Courses/Courses.jsx
+++ src/components/course/Courses/Courses.jsx
@@ -1,34 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './Courses.module.scss';
 import { Link } from 'react-router-dom';
 import data from '../../../dummy/data.json';
 
-const lecture = data.lecture;
+const SHOW_COUNT_DEFAULT = 3;
+const ALL_CATEGORIES_FILTER = '*';
 
 export default function Courses() {
-   const [showCount, setShowCount] = useState(4);
+   const [showCount, setShowCount] = useState(SHOW_COUNT_DEFAULT);
+   const [selectedCategory, setSelectedCategory] = useState(ALL_CATEGORIES_FILTER);
 
-   const handleShowMore = () => {
-      setShowCount(showCount + 4);
-   };
+   const courses = data.lecture;
+   console.log(courses);
+
+   const filteredCourses = courses?.filter(course => selectedCategory === ALL_CATEGORIES_FILTER || selectedCategory === course?.crs_category);
+
+   const handleShowMore = () => setShowCount(prevCount => prevCount + SHOW_COUNT_DEFAULT);
+
+   const handleFilter = category => setSelectedCategory(category);
+
+   const CourseItem = ({ course }) => (
+      <div className={styles.courses}>
+         <Link to={`/course/${course.id}`} key={course.id}>
+            <img className={styles.crs__image} src={course.thumbnail} alt={`courses ${course.id}`} />
+         </Link>
+         <div>
+            <span>{course.crs_category}</span>
+            <p>{course.tcr.tcr_name}</p>
+            <p>{course.crs_name}</p>
+            <p>{course.price}</p>
+         </div>
+      </div>
+   );
 
    return (
       <div className={styles.container}>
-         {lecture.slice(0, showCount).map(courses => (
-            <Link to={`/course/${courses.id}`} key={courses.id}>
-               <div className={styles.courses}>
-                  <img className={styles.crs__image} src={courses.thumbnail} alt={`courses ${courses.id}`} />
-                  <p>{courses.tcr.tcr_name}</p>
-                  <p>{courses.crs_name}</p>
-                  <p>{courses.price}</p>
+         <div className={styles.filterButtons}>
+            <button type='button' className={selectedCategory === '*' ? styles.active : ''} onClick={() => handleFilter('*')}>
+               모두
+            </button>
+            <button type='button' className={selectedCategory === '기초' ? styles.active : ''} onClick={() => handleFilter('기초')}>
+               기초
+            </button>
+            <button type='button' className={selectedCategory === '중급' ? styles.active : ''} onClick={() => handleFilter('중급')}>
+               중급
+            </button>
+            <button type='button' className={selectedCategory === '고급' ? styles.active : ''} onClick={() => handleFilter('고급')}>
+               고급
+            </button>
+         </div>
+
+         <div className={styles.course}>
+            {filteredCourses?.slice(0, showCount).map(course => (
+               <CourseItem key={course.id} course={course} />
+            ))}
+            {filteredCourses && showCount < filteredCourses.length && filteredCourses.length > SHOW_COUNT_DEFAULT && (
+               <div className={styles.showMore} onClick={handleShowMore}>
+                  더 보기
                </div>
-            </Link>
-         ))}
-         {showCount < lecture.length && (
-            <div className={styles.show__more} onClick={handleShowMore}>
-               더 보기
-            </div>
-         )}
+            )}
+         </div>
       </div>
    );
 }
--- src/pages/CourseList.jsx
+++ src/pages/CourseList.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import PortfolioSlid from '../components/course/Portfolio/PortfolioSild';
-import Search from '../components/course/Search/Search';
 import styles from './CourseList.module.scss';
 import CoursesLoader from '../components/course/Loader/CoursesLoader';
 import PortfoliosLoader from '../components/course/Loader/PortfolioLoader';
 import SearchLoader from '../components/course/Loader/SearchLoader';
+import Courses from '../components/course/Courses/Courses';
 
 export default function CourseList() {
    const [loading, setLoading] = useState(true);
@@ -28,7 +28,8 @@ export default function CourseList() {
          ) : (
             <>
                <PortfolioSlid />
-               <Search />
+               <Courses />
+               {/* <Search /> */}
             </>
          )}
       </div>
