3f51f5b0ed535a2b732685d9599da76aa91deb1e
selene-cho <selene.cho29@gmail.com>
2023-03-28 04:24:37 +0900

    feat: password edit logic ing...

--- src/pages/EditPassword.jsx
+++ src/pages/EditPassword.jsx
@@ -1,6 +1,31 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './EditPassword.module.scss';
+import { editPassword } from '../api/api';
 
 export default function EditPassword() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const [password, setPassword] = useState();
+
+  useEffect(() => {
+    setPassword();
+  }, []);
+
+  const savePassword = useMutation(editPassword, password, {
+    onSuccess: () => {
+      queryClient.refetchQueries(['myinfo']);
+      navigate('/');
+    },
+  });
+  console.log('savePassword', savePassword);
+
+  const onSavePassword = () => {
+    savePassword.mutate(password);
+  };
+
   return (
     <div className={styles.container}>
       <form>
@@ -12,7 +37,9 @@ export default function EditPassword() {
         />
         <input type="password" name="new_password" placeholder="새 비밀번호" />
         <input type="password" placeholder="새 비밀번호 확인" />
-        <button type="submit">변경 완료</button>
+        <button type="submit" onClick={onSavePassword}>
+          변경 완료
+        </button>
       </form>
     </div>
   );
